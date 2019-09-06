b0f5cc66e021be90301dad1cae9938fdc574a4d7
CIIDMike <39270867+belfie13@users.noreply.github.com>
2019-09-06 15:06:00 +1000

    Update CpanelDdns.php

--- CpanelDdns.php
+++ CpanelDdns.php
@@ -9,7 +9,7 @@
             --All Rights Reserved--
 
 ***********************************************/
-
+/********************************************************************************/
 require('lib/HttpRequest.php');
 
 /**
@@ -17,13 +17,35 @@ require('lib/HttpRequest.php');
  */
 class CpanelDdns
  {
-	private $_url;
-	private $_user;
-	private $_pass;
-	private $_token;
-
+	//~
 	/**
-	 * Update DDNS on your domain. This method requires a login, therefore be sure to provide the URL, username, and password before calling
+	 * 
+	 */
+	private $_url;
+	//~
+	/**
+	 * 
+	 */
+	private $_user;
+	//~
+	/**
+	 * 
+	 */
+	private $_pass;
+	//~
+	/**
+	 * 
+	 */
+	private $_token;
+	//~
+	//~
+	/**
+	 * Update DDNS on your domain. This method requires a login, therefore be
+	 * sure to provide the URL, username, and password before calling
+	 * 
+	 * expected url {@see https://documentation.cpanel.net/display/DD/cPanel+API+2+Functions+-+ZoneEdit%3A%3Aedit_zone_record}
+	 * 	https://hostname.example.com:2087/cpsess###########/json-api/cpanel?cpanel_jsonapi_user=user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=ZoneEdit&cpanel_jsonapi_func=edit_zone_record&Line=5&domain=example.com&name=sub&type=A&txtdata=v=blahblahblah&cname=example.com&address=10.10.10.10&ttl=14400&class=IN
+	 * 
 	 * @param  [String] $subdomain Your website's subdomain, used to connect to your VNC or whatever, e.g. "vnc"
 	 * @param  [String] $domain    Your website, e.g. "example.com"
 	 * @return                     Returns nothing
@@ -38,22 +60,26 @@ class CpanelDdns
 		$http = new HttpRequest($this->getUrl() . $this->getToken() . "/json-api/cpanel");
 		
 		$http
-			->setHeaders("Authorization: Basic " . base64_encode($this->getUser() . ":" . $this->getPass()) . "\n\r")
-			->get(array(
-				"address" => $_SERVER['REMOTE_ADDR'],
-				"class" => "IN",
-				"cpanel_jsonapi_func" => "edit_zone_record",
-				"cpanel_jsonapi_module" => "ZoneEdit",
-				"cpanel_jsonapi_version" => 2,
-				"domain" => $domain,
-				"line" => 28,
-				"name" => $subdomain . "." . $domain . ".",
-				"ttl" => 1200,
-				"type" => "A"
-			 ));
+			->setHeaders("Authorization: Basic " . 
+						 base64_encode($this->getUser() . ":" . $this->getPass()) . 
+						 "\n\r")
+			->get(
+				array(
+					"address" => $_SERVER['REMOTE_ADDR'],
+					"class" => "IN",
+					"cpanel_jsonapi_func" => "edit_zone_record",
+					"cpanel_jsonapi_module" => "ZoneEdit",
+					"cpanel_jsonapi_version" => 2,
+					"domain" => $domain,
+					"line" => 28,
+					"name" => $subdomain . "." . $domain . ".",
+					"ttl" => 1200,
+					"type" => "A"
+				)
+			);
 	}
-
-
+	//~
+	//~
 	/**
 	 * Login to your cpanel.
 	 * @return [Boolean] True if the login succeeded
@@ -68,10 +94,13 @@ class CpanelDdns
 		$params = "user=" . $user . "&pass=" . $pass;
 
 		$http = new HttpRequest($url . "/login");
-		$result = $http->post(array(
-			"user" => $user,
-			"pass" => $pass
-		 ));
+		
+		$result = $http->post(
+			array(
+				"user" => $user,
+				"pass" => $pass
+			)
+		);
 		
 		$inf = $http->getCurlInfo();
 		
@@ -85,48 +114,82 @@ class CpanelDdns
 			$this->setToken($cpsess[1]);
 			return true;
 		 }
-
-		return false;
+		else
+		 {
+			return false;
+		 }
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function getUrl()
 	 {
 		return $this->_url;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function setUrl($url)
 	 {
 		$this->_url = $url;
 		return $this;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function getUser()
 	 {
 		return $this->_user;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function setUser($user)
 	 {
 		$this->_user = $user;
 		return $this;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function getPass()
 	 {
 		return $this->_pass;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function setPass($pass)
 	 {
 		$this->_pass = $pass;
 		return $this;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function getToken()
 	 {
 		return $this->_token;
 	 }
 	//~
+	//~
+	/**
+	 * 
+	 */
 	function setToken($token)
 	 {
 		$this->_token = $token;
